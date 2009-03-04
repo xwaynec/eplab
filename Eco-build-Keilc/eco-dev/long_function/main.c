0f992ff62a9d49b1d3f3ff9adbecbb778c352b26
xwaynec <xwaynec@gmail.com>
2009-03-04 13:29:58 +0800

    add long function for testing

---
+++ Eco-build-Keilc/eco-dev/long_function/main.c
@@ -0,0 +1,64 @@
+#include "Eco/reg24e1.h"
+#include "Eco/eco_sys.h"
+#include "utils/utils.h"
+#include "serial/serial.h"
+#include "isr/isr_rf.h"
+#include "eeprom/eeprom.h"
+#include "eco_page.h"
+
+
+struct rf_config rf_data = { {0x00}, /* data2 width */
+	{0x38}, /* data1 width */ 
+	{0x00, 0x00, 0x00, 0x00, 0x00}, /* addr2 */
+	{0xF2, 0xF2, 0xF2, 0xF2, 0xF2}, /* addr1, host addr */
+	{0x63}, /* 24-bit address, 16-bit CRC */
+	{0x6f, 0xEC} };
+struct rf_config *cfg = &rf_data;
+char dst_addr[3] = {0xF1, 0xF1, 0xF1};
+char msg[7];
+
+
+extern unsigned char ECO_PAGE_SPI_CONN;
+/* rf.c
+void rf_init();
+void rf_configure(struct rf_config);
+void rf_send(char *, unsigned char, char *, unsigned char);
+*/
+
+int main()
+{
+	unsigned char idx;
+	unsigned int i = 0;
+	store_cpu_rate(16);
+	P0_DIR &= ~0x28;
+	P0_ALT &= ~0x28;
+	rf_init();
+	rf_configure(cfg);
+	serial_init(19200);	
+	eco_page_init();
+
+	msg[0] = 0x0A;
+	msg[1] = 0x00;
+	/* ADDR */
+	msg[2] = 0x02;
+	msg[3] = 0xA0;
+	/* LEN */
+	msg[4] = 0x02;
+	msg[5] = 0xDD;
+	msg[6] = 0xCC;
+	
+	for(idx = 4; idx > 0; idx--)
+	{
+		blink_led();		
+		mdelay(300);
+	}
+	mdelay(1000);	
+	while(1)
+	{
+		rf_send(dst_addr, 3, msg, 7);
+		blink_led();
+		mdelay(800);	
+	}
+
+	return 0;
+}
