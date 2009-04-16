1ec39fd7c3978d90efe5fa6b342b968291e56cc5
waynec <waynec@eplwiki.cs.nthu.edu.tw>
2009-04-16 23:29:10 +0800

    asm parser for eco system library v1.0b

--- Eco-build-Keilc/eco-dev/test_scalable_page_manager/tools/SRCCALL_Replacement.py
+++ Eco-build-Keilc/eco-dev/test_scalable_page_manager/tools/SRCCALL_Replacement.py
@@ -34,8 +34,10 @@ class SRCCall_Relocation:
     
         #translate function call
         self.PATTERN_FUNC_PARAMETER = '\s+MOV[\s]+R([\d]),(.+)'
-    
+
+
         self.asm_valid_funcs = []
+        self.asm_valid_lcall = []
    
         self._make_asm_function()
  
@@ -113,26 +115,146 @@ class SRCCall_Relocation:
 
     #######################################
     #   
-    #   eco page manager segment insert 
+    #   create asm function list 
     #
     #######################################
-    def add_page_manager_segment(self):
+    def create_asm_fuctions_list(self):
         print '---------------------------'
         print 'add eco page manager segment'
         print '---------------------------'
     
         #parse c comment function call
-        comment_template = string.Template(';[\s]+${func}\(([\w\s,]*)\)')
+        #comment_template = string.Template(';[\s]+${func}\(([\w\s,]*)\)')
+        #comment_template = string.Template('${func}\(([\w\s,]*)\);')
+        #comment_template = string.Template('[\s]${func}\(([\w\s,]*)\);')
+        #comment_template = string.Template(';[=\(\w\s]*${func}\(([\w\s,]*)\)')
+        comment_template = string.Template(';[=\(\w\s]*${func}\(([\w\s\>\<&,]*)\)')
+        
+        lcall_template = string.Template('LCALL[\s]+(${func})')
 
         for i in range(len(self.ECO_ASM_FUNC)):
             if self.ECO_ASM_FUNC[i][0:1] == '_':
                 self.asm_valid_funcs.append(comment_template.substitute(func = self.ECO_ASM_FUNC[i][1:]))
             else:
                 self.asm_valid_funcs.append(comment_template.substitute(func = self.ECO_ASM_FUNC[i])) 
-   
-       
-        print self.asm_valid_funcs 
+  
+
+        for i in range(len(self.ECO_ASM_FUNC)):
+            self.asm_valid_lcall.append(lcall_template.substitute(func = self.ECO_ASM_FUNC[i])) 
+        
  
+        print self.asm_valid_lcall 
+
+
+    #######################################
+    #
+    #   parse commnet c function 
+    #
+    #######################################
+    def parse_lcall_function(self):
+        print '---------------------------'
+        print 'parse c function'
+        print '---------------------------'
+
+        f = open(self.src_file,'r')
+        asm_lines = f.readlines()
+        #print asm_lines
+        f.close()       
+
+        modify_start_index = 0
+        modify_end_index = 0
+        parameter_flag = False
+ 
+        #for line in asm_lines:
+        for line_index in range(len(asm_lines)):
+            for valid_func in self.asm_valid_lcall:
+                reg_lcall_function = re.search(valid_func,asm_lines[line_index])
+
+                if reg_lcall_function:
+                    #print 'reg_lcall_function.group(1) = ',reg_lcall_function.group(1)
+                    modify_end_index = line_index
+                    for i in range(line_index,0,-1):
+                        reg_c_function = re.search(self.asm_valid_funcs[self.asm_valid_lcall.index(valid_func)],asm_lines[i])
+                        if reg_c_function:
+                            if reg_c_function.group(1):
+                                #print 'this function has parameter'
+                                parameter_flag = True
+                            else:
+                                #print 'this function has no parameter'
+                                parameter_flag = False
+                            modify_start_index = i
+                            #print 'lcall %s \nfunction %s\nit starts at %d line and ends at %d\n\n' %(reg_c_function.group(0),reg_lcall_function.group(0) ,i,line_index)
+                            break
+ 
+                    if parameter_flag:
+                        print 'YES modify_start_index = %d and modify_end_index = %d' %(modify_start_index,modify_end_index)
+                        parameter_index = 0
+                        for index in range(modify_start_index,modify_end_index+1):
+                            #self.PATTERN_FUNC_PARAMETER = '\s+MOV[\s]+R([\d]),(.+)'
+                            reg_func_para = re.search(self.PATTERN_FUNC_PARAMETER,asm_lines[index])
+                            if reg_func_para:
+                                #print '\t;' + asm_lines[index]
+                                #print '\tMOV\tR0,#LOW (ECO_PAGE_REGISTER%s)\n' %reg_func_para.group(1)
+                                #print '\tMOV\t@R0,%s\n' %reg_func_para.group(2)
+                                temp = '\t;----------start----------\r\n' 
+                                temp += '\t;' + asm_lines[index] + '\r\n'
+                                temp += '\tMOV\tR0,#LOW (ECO_PAGE_REGISTER%s)\r\n' %reg_func_para.group(1)
+                                temp += '\tMOV\t@R0,%s\r\n' %reg_func_para.group(2)
+                                temp += '\t;----------end----------\r\n' 
+                                
+                                asm_lines.pop(index)
+                                asm_lines.insert(index,temp)
+                                
+                                #asm_lines.insert(index,'\tMOV\t@R0,%s\n' %reg_func_para.group(2))
+                                #asm_lines.insert(index,'\tMOV\tR0,#LOW (ECO_PAGE_REGISTER%s)\n' %reg_func_para.group(1))
+                                #asm_lines.insert(index,temp)
+                                #for index in range(modify_start_index,modify_end_index+1):
+                                #    print asm_lines[index] 
+                            #else:
+                            #    print asm_lines[index]
+                      
+                        temp = '\t;YES----------start----------\r\n' 
+                        temp += ';' + asm_lines[modify_end_index] + '\r\n'
+                        temp += '\tMOV\tECO_PAGE_ADDR,#HIGH (%s)\r\n' %reg_lcall_function.group(1)
+                        temp += '\tMOV\tECO_PAGE_ADDR,#LOW (%s)\r\n' %reg_lcall_function.group(1)
+                        temp += '\tLCALL\teco_page_manager\r\n'
+                        temp += '\t;YES----------end----------\n\n'
+                        asm_lines.pop(modify_end_index) 
+                        asm_lines.insert(modify_end_index,temp)
+
+                        #asm_lines.insert(modify_end_index+1,'\tMOV\tECO_PAGE_ADDR,#HIGH (%s)' %reg_lcall_function.group(1))
+                        #asm_lines.insert(modify_end_index+2,'\tMOV\tECO_PAGE_ADDR,#LOW (%s)' %reg_lcall_function.group(1))
+                        #asm_lines.insert(modify_end_index+3,'\tLCALL\teco_page_manager')
+
+                        for index in range(modify_start_index,modify_end_index+1):
+                            print asm_lines[index] 
+                        
+          
+ 
+                    
+                    else:
+                        print 'NO modify_start_index = %d and modify_end_index = %d' %(modify_start_index,modify_end_index)
+                        #print '\tMOV\tECO_PAGE_ADDR,#HIGH (%s)' %reg_lcall.group(1)
+                        #print '\tMOV\tECO_PAGE_ADDR+01H,#LOW (%s)' %reg_lcall.group(1)
+                        #print '\tLCALL\teco_page_manager'
+                        temp = '\t;NO----------start----------\r\n' 
+                        temp += ';' + asm_lines[modify_end_index] + '\r\n'
+                        temp += '\tMOV\tECO_PAGE_ADDR,#HIGH (%s)\r\n' %reg_lcall_function.group(1)
+                        temp += '\tMOV\tECO_PAGE_ADDR,#LOW (%s)\r\n' %reg_lcall_function.group(1)
+                        temp += '\tLCALL\teco_page_manager\r\n'
+                        temp += '\t;NO----------end----------\n\n'
+                        asm_lines.pop(modify_end_index) 
+                        asm_lines.insert(modify_end_index,temp)
+
+                        #asm_lines.insert(modify_end_index+1,'\tMOV\tECO_PAGE_ADDR,#HIGH (%s)' %reg_lcall_function.group(1))
+                        #asm_lines.insert(modify_end_index+2,'\tMOV\tECO_PAGE_ADDR,#LOW (%s)' %reg_lcall_function.group(1))
+                        #asm_lines.insert(modify_end_index+3,'\tLCALL\teco_page_manager')
+
+                        for index in range(modify_start_index,modify_end_index+1):
+                            print asm_lines[index] 
+                        
+        #f.close()
+
 
 
 if __name__ == '__main__':
@@ -146,5 +268,7 @@ if __name__ == '__main__':
 
     src.parse_function_call()
      
-    src.add_page_manager_segment() 
+    src.create_asm_fuctions_list()
+
+    src.parse_lcall_function()
  
